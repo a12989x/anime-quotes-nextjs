3979cdcc989ccd36a45273becaa7b15665ea0b64
Alexis Guzman <alexis12989x@gmail.com>
2021-08-27 12:48:48 -0500

    chore(imports): change normal imports from absolute imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
+import Layout from '@/components/layout';
+import QuoteContextProvider from '@/context/QuoteContext';
+import '@/styles/globals.scss';
 import { AppProps } from 'next/app';
-import QuoteContextProvider from '../context/QuoteContext';
-import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <QuoteContextProvider>
-      <Component {...pageProps} />
-    </QuoteContextProvider>
+    <Layout>
+      <QuoteContextProvider>
+        <Component {...pageProps} />
+      </QuoteContextProvider>
+    </Layout>
   );
 };
 
