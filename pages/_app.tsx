638e19c9a2b0967cb16ca0e1a41b69c5cb1485b4
Alexis Guzman <alexis12989x@gmail.com>
2021-08-27 09:56:48 -0500

    feat(context): import quote context provider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app';
+import QuoteContextProvider from '../context/QuoteContext';
 import '../styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
-  return <Component {...pageProps} />;
+  return (
+    <QuoteContextProvider>
+      <Component {...pageProps} />
+    </QuoteContextProvider>
+  );
 };
 
 export default MyApp;
