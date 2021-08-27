fa840ec38e289fb0c7cf8a10ef3754e9349d6bb9
Alexis Guzman <alexis12989x@gmail.com>
2021-08-27 17:49:51 -0500

    feat(next-themes): import theme provider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import Layout from '@/components/layout';
 import QuoteContextProvider from '@/context/QuoteContext';
 import '@/styles/globals.scss';
+import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <Layout>
-      <QuoteContextProvider>
-        <Component {...pageProps} />
-      </QuoteContextProvider>
-    </Layout>
+    <ThemeProvider defaultTheme='system'>
+      <Layout>
+        <QuoteContextProvider>
+          <Component {...pageProps} />
+        </QuoteContextProvider>
+      </Layout>
+    </ThemeProvider>
   );
 };
 
