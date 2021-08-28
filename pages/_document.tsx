d6caad838fa296162ee2c5e9f96ad171ea712774
Alexis Guzman <alexis12989x@gmail.com>
2021-08-28 14:20:03 -0500

    feat(seo): add meta tags

---
+++ pages/_document.tsx
@@ -0,0 +1,55 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link href='/favicons/favicon.ico' rel='shortcut icon' />
+          <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/favicons/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicons/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicons/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/favicons/site.webmanifest' />
+          <link
+            rel='mask-icon'
+            href='/favicons/safari-pinned-tab.svg'
+            color='#805ad5'
+          />
+          <meta name='msapplication-TileColor' content='#805ad5' />
+          <meta name='theme-color' content='#805ad5' />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
