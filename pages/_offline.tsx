8b04ea0ca901e0854743a6a795379fb12f443d17
Alexis Guzman <alexis12989x@gmail.com>
2021-08-28 14:22:42 -0500

    feat(page): create and set up offline page component

---
+++ pages/_offline.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+
+const Offline: NextPage = () => {
+  return (
+    <main>
+      <h1>You are offline</h1>
+      <p>Please check your internet connection.</p>
+    </main>
+  );
+};
+
+export default Offline;
