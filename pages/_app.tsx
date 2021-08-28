35ba5e1adab4b127bfa9ef119131a11970ff48f7
Alexis Guzman <alexis12989x@gmail.com>
2021-08-28 14:07:35 -0500

    feat(seo): import default seo component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import Head from '@/components/Head';
 import Layout from '@/components/layout';
 import QuoteContextProvider from '@/context/QuoteContext';
 import '@/styles/globals.scss';
+import { DefaultSeo } from 'next-seo';
 import { ThemeProvider } from 'next-themes';
 import { AppProps } from 'next/app';
+import SEO from '../next-seo.config';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head />
+      <DefaultSeo {...SEO} />
       <ThemeProvider defaultTheme='system'>
         <Layout>
           <QuoteContextProvider>
