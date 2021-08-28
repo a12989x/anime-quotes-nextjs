b7199a904bb969faff1aee762959748ca8200c7a
Alexis Guzman <alexis12989x@gmail.com>
2021-08-28 11:37:09 -0500

    feat(context): update initial state

--- store/reducers/quoteReducer.tsx
+++ store/reducers/quoteReducer.tsx
@@ -1,7 +1,12 @@
 import { IQuote } from '../../types';
 import { QuoteActionsType, UPDATE_QUOTE } from '../actions/quoteActions';
 
-export const initialState: IQuote = { quote: '', anime: '', character: '' };
+export const initialState: IQuote = {
+  quote:
+    "Life is a succession of committing sin. Life is evil itself. I'm conscious that I'm evil. And so are all of you.",
+  anime: 'Tokyo Ghoul √A',
+  character: 'Yoshimura',
+};
 
 const quoteReducer = (store: IQuote, action: QuoteActionsType) => {
   switch (action.type) {
